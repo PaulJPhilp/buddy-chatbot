73baa6c5270c5ea76e521a02af1922c5fe2e907f
PaulJPhilp <138239846+PaulJPhilp@users.noreply.github.com>
2025-02-09 14:44:23 -0500

    added google, anthropic and new openai models.

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,4 +1,6 @@
 import { openai } from '@ai-sdk/openai';
+import { anthropic } from "@ai-sdk/anthropic";
+import { google } from '@ai-sdk/google';
 import { fireworks } from '@ai-sdk/fireworks';
 import {
   customProvider,
@@ -6,18 +8,21 @@ import {
   wrapLanguageModel,
 } from 'ai';
 
-export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
+export const DEFAULT_CHAT_MODEL: string = 'openai-small';
 
 export const myProvider = customProvider({
   languageModels: {
-    'chat-model-small': openai('gpt-4o-mini'),
-    'chat-model-large': openai('gpt-4o'),
-    'chat-model-reasoning': wrapLanguageModel({
+    'openai-small': openai('gpt-4o-mini'),
+    'openai-large': openai('gpt-4o'),
+    'openai-reasoning': openai('o3-mini'),
+    'openseek-R1': wrapLanguageModel({
       model: fireworks('accounts/fireworks/models/deepseek-r1'),
       middleware: extractReasoningMiddleware({ tagName: 'think' }),
     }),
     'title-model': openai('gpt-4-turbo'),
     'block-model': openai('gpt-4o-mini'),
+    'coding-model': anthropic('claude-3-5-sonnet'),
+    'google-flashmodel': google('gemini-2.0-flash'),
   },
   imageModels: {
     'small-model': openai.image('dall-e-2'),
@@ -33,18 +38,33 @@ interface ChatModel {
 
 export const chatModels: Array<ChatModel> = [
   {
-    id: 'chat-model-small',
+    id: 'openai-small',
     name: 'Small model',
     description: 'Small model for fast, lightweight tasks',
   },
   {
-    id: 'chat-model-large',
+    id: 'openai-large',
     name: 'Large model',
     description: 'Large model for complex, multi-step tasks',
   },
   {
-    id: 'chat-model-reasoning',
-    name: 'Reasoning model',
+    id: 'openai-reasoning',
+    name: 'OpenAI Reasoning model',
+    description: 'Large reasoning model for complex, multi-step tasks',
+  },
+  {
+    id: 'openseek-R1',
+    name: 'OpenSeek R1 model',
     description: 'Uses advanced reasoning',
+  },
+  {
+    id: 'coding-model',
+    name: 'Coding model',
+    description: 'Uses advanced coding',
+  },
+  {
+    id: 'google-flashmodel',
+    name: 'Google flash model',
+    description: 'Fast, lightweight model',
   },
 ];
