1d65eb5faaa0e2b0a840851b738bc902494fb3f3
PaulJPhilp <138239846+PaulJPhilp@users.noreply.github.com>
2025-02-14 11:45:03 -0500

    feat:  add vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react"
+
 import type { Metadata } from 'next';
 import { Toaster } from 'sonner';
 
@@ -65,6 +67,7 @@ export default async function RootLayout({
         >
           <Toaster position="top-center" />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
